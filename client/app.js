4a0f5d2c212c9cb56cd899b4178a28d129c31d04
zigzag8881 <ninabarnabishvili.viidakko@gmail.com>
2020-07-12 20:13:54 +0300

    Logic improved to use single source of data

--- client/app.js
+++ client/app.js
@@ -1,197 +1,253 @@
 var map;
-var marker;
+var currentMarker;
 var userMarkers = {};
 
 function initMap() {
-  var helsinki = new google.maps.LatLng(60.169, 24.938);
+	var helsinki = new google.maps.LatLng(60.169, 24.938);
 
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: helsinki,
-    zoom: 12,
-  });
+	var map = new google.maps.Map(document.getElementById("map"), {
+		center: helsinki,
+		zoom: 12,
+	});
 
-  Object.values(userMarkers).forEach((marker) => {
-    marker.setMap(map);
-  });
+	Object.values(userMarkers).forEach((markerItem) => {
+		markerItem.setMap(map);
+	});
 
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById("search-input");
-  var searchBox = new google.maps.places.SearchBox(input);
-  //   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+	// Create the search box and link it to the UI element.
+	var input = document.getElementById("search-input");
+	var searchBox = new google.maps.places.SearchBox(input);
+	//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener("bounds_changed", function () {
-    searchBox.setBounds(map.getBounds());
-  });
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener("bounds_changed", function () {
+		searchBox.setBounds(map.getBounds());
+	});
 
-  var markers = [];
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", function () {
-    var places = searchBox.getPlaces();
+	var markers = [];
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener("places_changed", function () {
+		var places = searchBox.getPlaces();
 
-    if (places.length == 0) {
-      return;
-    }
+		if (places.length == 0) {
+			return;
+		}
 
-    // Clear out the old markers.
-    markers.forEach(function (marker) {
-      marker.setMap(null);
-    });
-    markers = [];
+		// Clear out the old markers.
+		markers.forEach(function (markerItem) {
+			markerItem.setMap(null);
+		});
+		markers = [];
 
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function (place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
+		// For each place, get the icon, name and location.
+		var bounds = new google.maps.LatLngBounds();
+		places.forEach(function (place) {
+			if (!place.geometry) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
+			var icon = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25),
+			};
 
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map: map,
-          icon: icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
+			// Create a marker for each place.
+			markers.push(
+				new google.maps.Marker({
+					map: map,
+					icon: icon,
+					title: place.name,
+					position: place.geometry.location,
+				})
+			);
 
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+	});
 
-  // Add place on map
-  map.addListener("click", function (e) {
-    const marker = placeMarkerAndPanTo(e.latLng, map);
-    const markerIndex = Object.keys(userMarkers).length;
-    marker.addListener("click", function () {
-      infowindow.setContent(
-        "<div>" +
-          "<div id='editableMarker'><strong><p id='place-title'>New place</p></strong><br>" +
-          "<p id='place-desc'>Description</p>" +
-          "<p id='map-coord'>Map coordinates</p>" +
-          "<p id='open-hours'>Opening hours</p>" +
-          "</div>" +
-          "<div id='btn-add'><button onclick='addToFav()' class='button-add'>+</button></div>" +
-          "<div id='btn-delete'><button id='marker-" +
-          markerIndex +
-          "' onclick='deleteMarker(event)' class='button-delete'>-</button></div>" +
-          "<div id='btn-edit'><button onclick='editMarker()' class='button-edit'>Edit</button></div>" +
-          "<div id='btn-save'><button onclick='saveMarker()' class='button-save'>Save</button></div>" +
-          "</div>"
-      );
-      infowindow.open(map, marker);
-    });
-  });
+	// Add place on map
+	map.addListener("click", function (e) {
+		infowindow.close();
+		var localCurrentMarker = updateCurrentMarker(e.latLng, map);
+		currentMarker = localCurrentMarker;
+		var markerIndex = Object.keys(userMarkers).length;
+		localCurrentMarker.addListener("click", function () {
+			infowindow.setContent(
+				"<div>" +
+					"<div id='editableMarker'><strong><p id='place-title'>" +
+					localCurrentMarker.title +
+					"</p></strong><br>" +
+					"<p id='place-desc'>" +
+					localCurrentMarker.description +
+					"</p>" +
+					"<p id='map-coord'>" +
+					localCurrentMarker.position +
+					"</p>" +
+					"<p id='open-hours'>" +
+					localCurrentMarker.openingHours +
+					"</p>" +
+					"</div>" +
+					"<div id='btn-add'><button id='marker-" +
+					markerIndex +
+					"' onclick='addToFav(event)' class='button-add'>+</button></div>" +
+					"<div id='btn-delete'><button id='marker-" +
+					markerIndex +
+					"' onclick='deleteMarker(event)' class='button-delete'>-</button></div>" +
+					"<div id='btn-edit'><button onclick='editMarker()' class='button-edit'>Edit</button></div>" +
+					"<div id='btn-save'><button id='marker-" +
+					markerIndex +
+					"' onclick='saveMarker(event)' class='button-save'>Save</button></div>" +
+					"</div>"
+			);
+			infowindow.open(map, localCurrentMarker);
+		});
+	});
 
-  var infowindow = new google.maps.InfoWindow();
+	var infowindow = new google.maps.InfoWindow();
 }
 
-function placeMarkerAndPanTo(latLng, map) {
-  if (marker !== undefined && !Object.values(userMarkers).includes(marker)) {
-    marker.setMap(null);
-    marker = undefined;
-  }
-  marker = new google.maps.Marker({
-    position: latLng,
-    map: map,
-    title: "New place",
-  });
-
-  return marker;
+function updateCurrentMarker(latLng, map) {
+	if (
+		currentMarker !== undefined &&
+		!Object.values(userMarkers).includes(currentMarker)
+	) {
+		currentMarker.setMap(null);
+		currentMarker = undefined;
+	}
+	return new google.maps.Marker({
+		position: latLng,
+		map: map,
+		title: "New place",
+		description: "Description",
+		openingHours: "Opening Hours",
+	});
 }
 
 function deleteMarker(e) {
-  console.log(e.target.id);
-  let { id } = e.target;
-  const markerIdStart = id.indexOf("-") + 1;
-  id = id.substring(markerIdStart, id.length);
+	let { id } = e.target;
+	const markerIdStart = id.indexOf("-") + 1;
+	id = id.substring(markerIdStart, id.length);
 
-  userMarkers[id].setMap(null);
-  delete userMarkers[id];
+	userMarkers[id].setMap(null);
+	delete userMarkers[id];
+	updateFav();
 }
 
-function addToFav() {
-  if (!Object.values(userMarkers).includes(marker)) {
-    userMarkers[Object.keys(userMarkers).length] = marker;
-    const node = document.getElementById("places");
+function addToFav(e) {
+	let markerIndex = e.target.id; // marker-1
+	const markerIdStart = markerIndex.indexOf("-") + 1; // 7
+	markerIndex = markerIndex.substring(markerIdStart, markerIndex.length); // 1
 
-    marker.title = localStorage.getItem("place-title");
+	if (
+		!userMarkers[markerIndex] &&
+		!Object.values(userMarkers).includes(currentMarker)
+	) {
+		userMarkers[Object.keys(userMarkers).length] = currentMarker;
+		updateFav();
+	}
+}
 
-    const childNode = document.createElement("li");
-    const textNode = document.createTextNode(marker.title);
-    childNode.appendChild(textNode);
-    node.appendChild(childNode);
-  }
+function updateFav() {
+	const node = document.getElementById("places");
+
+	while (node.firstChild) {
+		node.removeChild(node.firstChild);
+	}
+
+	Object.values(userMarkers).forEach((markerItem) => {
+		const childNode = document.createElement("li");
+		const textNode = document.createTextNode(markerItem.title);
+		childNode.appendChild(textNode);
+		node.appendChild(childNode);
+	});
 }
 
 function editMarker() {
-  var btnToggle = document.getElementById("btn-edit");
+	var btnToggle = document.getElementById("btn-edit");
 
-  if (btnToggle.style.display !== "none") {
-    btnToggle.style.display = "none";
-    btnToggle = document.getElementById("btn-save");
-    btnToggle.style.display = "block";
-    document.getElementById("editableMarker").contentEditable = true;
-    document.addEventListener("keydown", logKey);
-  } else {
-    btnToggle.style.display = "block";
-  }
+	if (btnToggle.style.display !== "none") {
+		btnToggle.style.display = "none";
+		btnToggle = document.getElementById("btn-save");
+		btnToggle.style.display = "block";
+		document.getElementById("editableMarker").contentEditable = true;
+		document.addEventListener("keydown", logKey);
+	} else {
+		btnToggle.style.display = "block";
+	}
 }
 
-function saveMarker() {
-  var btnToggle = document.getElementById("btn-save");
-  var editables = document.querySelectorAll(
-    "#place-title, #place-desc, #map-coord, #open-hours"
-  );
+function saveMarker(e) {
+	let markerIndex = e.target.id; // marker-1
+	const markerIdStart = markerIndex.indexOf("-") + 1; // 7
+	markerIndex = markerIndex.substring(markerIdStart, markerIndex.length); // 1
 
-  if (btnToggle.style.display !== "none") {
-    btnToggle.style.display = "none";
-    btnToggle = document.getElementById("btn-edit");
-    btnToggle.style.display = "block";
-    document.getElementById("editableMarker").contentEditable = false;
-    for (var i = 0; i < editables.length; i++) {
-      localStorage.setItem(
-        editables[i].getAttribute("id"),
-        editables[i].innerHTML
-      );
-    }
-  } else {
-    btnToggle.style.display = "block";
-  }
+	var btnToggle = document.getElementById("btn-save");
+	var props = document.querySelectorAll(
+		"#place-title, #place-desc, #map-coord, #open-hours"
+	);
+
+	console.log("asd");
+
+	if (btnToggle.style.display !== "none") {
+		btnToggle.style.display = "none";
+		btnToggle = document.getElementById("btn-edit");
+		btnToggle.style.display = "block";
+		document.getElementById("editableMarker").contentEditable = false;
+		var markerRef = userMarkers[markerIndex] ?? currentMarker;
+
+		for (var i = 0; i < props.length; i++) {
+			var contentName = props[i].getAttribute("id");
+			var content = props[i].innerHTML;
+
+			switch (contentName) {
+				case "place-title":
+					markerRef.title = content;
+					break;
+				case "place-desc":
+					markerRef.description = content;
+					break;
+				case "map-coord":
+					markerRef.position = content;
+					break;
+				case "open-hours":
+					markerRef.openingHours = content;
+					break;
+				default:
+					break;
+			}
+		}
+	} else {
+		btnToggle.style.display = "block";
+	}
+	updateFav();
 }
 
 // Collapse Favourites in left panel
 
 function collapsePlaces() {
-  var btnToggle = document.getElementById("left-panel-places");
-  if (btnToggle.style.display !== "none") {
-    btnToggle.style.display = "none";
-  } else {
-    btnToggle.style.display = "block";
-  }
+	var btnToggle = document.getElementById("left-panel-places");
+	if (btnToggle.style.display !== "none") {
+		btnToggle.style.display = "none";
+	} else {
+		btnToggle.style.display = "block";
+	}
 }
 
-// Prevent add empty sapce for separate id
+// Prevent adding empty space for separate id
 
 function logKey(e) {
 	if (e.keyCode === 13) {
-	  document.execCommand("insertHTML", false, "<br/>");
-	  return true;
+		document.execCommand("insertHTML", false, "<br/>");
+		return true;
 	}
-  }
+}
